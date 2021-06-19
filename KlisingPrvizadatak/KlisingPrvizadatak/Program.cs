f780fff45d848eea2e12c2088d9b469281ae8d85
filip kotur <43764880+filipkotur@users.noreply.github.com>
2021-06-19 23:56:58 +0200

    Add files via upload

--- KlisingPrvizadatak/KlisingPrvizadatak/Program.cs
+++ KlisingPrvizadatak/KlisingPrvizadatak/Program.cs
@@ -11,54 +11,59 @@ namespace KlisingPrvizadatak
 
             while (1 == 1)
             {
-                int counter = 0;
+                int brojač = 0;
                 Console.WriteLine("Unesi niz brojeva: ");
-                string name = Console.ReadLine();
-                string[] podijelaRijeci = name.Split(" ");
+                string pocetniUnos = Console.ReadLine();
+                if(!string.IsNullOrEmpty(pocetniUnos)) {
+
+                string[] podijelaRijeci = pocetniUnos.Split(" ");
                 int[] nizbrojeva = new int[podijelaRijeci.Length];
-
-                foreach (string s in podijelaRijeci)
+                foreach (string rijec in podijelaRijeci)
                 {
-                    if (Int32.TryParse(s, out broj))
-                        nizbrojeva[counter++] = broj;
-                    else
-                    {
-
-                        Console.WriteLine("Nisi unio brojeve!");
-                        break;
-                    }
-                }
-                //do ovdje je zapisano unos broj i provjera nakon toga slijedi algoritam 
-                if (podijelaRijeci.Length == nizbrojeva.Length)
-                {
-                    Console.WriteLine("Unesi broj koji je zbroj dvaju brojeva: ");
-
-                    string stringProvjera = Console.ReadLine();
-                    int tocanBroj;  provjera = int.TryParse(stringProvjera, out tocanBroj);
-                    if (provjera == true)
-                    {
-                        int pokazivač = 0;
-                        //ALGORITAM
-                        foreach (int a in nizbrojeva)
+                                     
+                        if (Int32.TryParse(rijec, out broj))
+                            nizbrojeva[brojač++] = broj;
+                        else
                         {
-                            for (int i = pokazivač + 1; i < nizbrojeva.Length; i++)
-                            {
-                                if (a + nizbrojeva[i] == tocanBroj)
-                                {
-                                    Console.WriteLine(a + "," + nizbrojeva[i]);
-
-
-                                }
-                            }
-                            pokazivač++;
-                        }
+                        Console.WriteLine("Nisi unio brojeve!");
+                      
                     }
-                    else
+
+                }
+
+
+
+                    //do ovdje je zapisano unos broj i provjera nakon toga slijedi algoritam 
+                    if (podijelaRijeci.Length == nizbrojeva.Length)
                     {
-                        Console.WriteLine("Nisi unio broj");
+                        Console.WriteLine("Unesi broj koji je zbroj dvaju brojeva: ");
+
+                        string stringProvjera = Console.ReadLine();
+                        int tocanBroj; provjera = int.TryParse(stringProvjera, out tocanBroj);
+                        if (provjera == true)
+                        {
+                            int pokazivač = 0;
+                            //ALGORITAM
+                            foreach (int brojuNizu in nizbrojeva)
+                            {
+                                for (int i = pokazivač + 1; i < nizbrojeva.Length; i++)
+                                {
+                                    if (brojuNizu + nizbrojeva[i] == tocanBroj)
+                                    {
+                                        Console.WriteLine(brojuNizu + "," + nizbrojeva[i]);
+
+
+                                    }
+                                }
+                                pokazivač++;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nisi unio broj");
+                        }
+
                     }
-
-
                 }
 
 
