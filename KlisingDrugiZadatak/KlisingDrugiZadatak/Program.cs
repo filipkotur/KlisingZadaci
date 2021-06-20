da92c75a7cf6c7cee4ea5477a98a4dc2b0b0d1a3
filip kotur <43764880+filipkotur@users.noreply.github.com>
2021-06-20 18:28:27 +0200

    Add files via upload

--- KlisingDrugiZadatak/KlisingDrugiZadatak/Program.cs
+++ KlisingDrugiZadatak/KlisingDrugiZadatak/Program.cs
@@ -10,40 +10,43 @@ namespace KlisingDrugiZadatak
 
             while (1 == 1)
             {
-                int counter = 0;
+                int brojač = 0;
                 Console.WriteLine("Unesi niz brojeva: ");
-                string recenica = Console.ReadLine();
-                string[] podijelaRijeci = recenica.Split(" ");
-                int[] nizbrojeva = new int[podijelaRijeci.Length];
-
-                foreach (string s in podijelaRijeci)
+                string pocetniUnos = Console.ReadLine();
+                if (!string.IsNullOrEmpty(pocetniUnos))
                 {
-                    if (Int32.TryParse(s, out broj))
-                        nizbrojeva[counter++] = broj;
-                    else
+                    string[] podijelaRijeci = pocetniUnos.Split(" ");
+                    int[] nizbrojeva = new int[podijelaRijeci.Length];
+
+                    foreach (string rijec in podijelaRijeci)
+                    {
+                        if (Int32.TryParse(rijec, out broj))
+                            nizbrojeva[brojač++] = broj;
+                        else
+                        {
+
+                            Console.WriteLine("Nisi unio brojeve!");
+                            break;
+                        }
+                    }
+                    //do ovdje je zapisano unos broj i provjera nakon toga slijedi algoritam 
+                    if (podijelaRijeci.Length == nizbrojeva.Length)
                     {
 
-                        Console.WriteLine("Nisi unio brojeve!");
-                        break;
+                        int[] noviNiz = new int[nizbrojeva.Length];
+                        int krajReda = nizbrojeva.Length - 1;
+                        //ALGORITAM
+                        foreach (int brojuNizu in nizbrojeva)
+                        {
+                            noviNiz[krajReda] = brojuNizu;
+                            krajReda--;
+                        }
+                        foreach (int brojuNizu in noviNiz)
+                        {
+                            Console.Write(brojuNizu + " ");
+                        }
+                        Console.WriteLine();
                     }
-                }
-                //do ovdje je zapisano unos broj i provjera nakon toga slijedi algoritam 
-                if (podijelaRijeci.Length == nizbrojeva.Length)
-                {
-
-                    int[] noviNiz = new int[nizbrojeva.Length];
-                    int krajReda = nizbrojeva.Length -1;
-                    //ALGORITAM
-                    foreach (int a in nizbrojeva)
-                    {
-                        noviNiz[krajReda] = a;
-                        krajReda--;
-                    }
-                    foreach (int a in noviNiz)
-                    {
-                        Console.Write(a + " ");
-                    }
-                    Console.WriteLine();
                 }
             }
         }
